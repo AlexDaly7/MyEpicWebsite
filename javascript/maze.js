078ef972a522100826e769eafb5ae3086f19ec3e
Refired <alexshepherdno1@gmail.com>
2024-12-27 15:06:03 +0000

    Maze system

--- javascript/maze.js
+++ javascript/maze.js
@@ -1,21 +1,22 @@
-function mazeFunc(state) {
-	var url = window.location.pathname;
-	var index = url.lastIndexOf("/") + 1;
-	var name = url.substr(index);
-	alert(name);
-	if (state == "loadNew") {
-		localStorage.setItem("page", name);
-		alert("set new page");
-	}
-	
-	if (state == "check") {
-		pageStored = localStorage.getItem("page");
-		if (pageStored != name) {
-			alert("hi");
-		} else {
-			
-			
-		}
-	}
+// Variables at start to work for any occasion.
 
+var url = window.location.pathname;
+var index = url.lastIndexOf("/") + 1;
+var name = url.substr(index);
+
+function loadNew() {
+	
+	//localStorage.setItem("page", name);
+	alert("set new page " + name);
+	alert(localStorage.getItem("page"));
+
+}
+
+function checkMaze() {
+	pageStored = localStorage.getItem("page");
+	alert(pageStored + " | " + name);
+	if (pageStored != name) {
+		alert(pageStored + " this means it is not the same");	
+	
+	}
 }
