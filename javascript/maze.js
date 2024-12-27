a8a6ead798f56414f39c67464b744c215f246d5c
Refired <alexshepherdno1@gmail.com>
2024-12-27 16:41:05 +0000

    Working maze system

--- javascript/maze.js
+++ javascript/maze.js
@@ -8,28 +8,17 @@ pageUrl = url.substr(0, index);
 
 
 function loadNew() {
-	
 	localStorage.setItem("page", name);
-	alert("set new page " + name);
-	alert(localStorage.getItem("page"));
-	
 }
 
 function checkMaze() {
-	
-	alert(pageStored + " | " + name);
-	
 	if (pageStored != name) {
-		alert(pageStored + " this means it is not the same");	
-		alert(pageUrl);
 		location.replace(pageUrl + pageStored);
 	}
 }
 
 function goToMaze(setPage) {
 	pageToGo = pageUrl + setPage;
-	alert("hi");
 	localStorage.setItem("page", setPage);
 	location.replace(pageToGo);
-	alert("page changed");
 }
