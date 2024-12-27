bd51e5ff8e150f051e6e217849b4c6bbac1877c3
Refired <alexshepherdno1@gmail.com>
2024-12-27 17:31:20 +0000

    comments

--- javascript/maze.js
+++ javascript/maze.js
@@ -6,17 +6,19 @@ var name = url.substr(index);
 pageStored = localStorage.getItem("page");
 pageUrl = url.substr(0, index);
 
-
+// Change stored to current page
 function loadNew() {
 	localStorage.setItem("page", name);
 }
 
+// Check current page against stored
 function checkMaze() {
 	if (pageStored != name) {
 		location.replace(pageUrl + pageStored);
 	}
 }
 
+// Changes page to the next page and changes to that page
 function goToMaze(setPage) {
 	pageToGo = pageUrl + setPage;
 	localStorage.setItem("page", setPage);
